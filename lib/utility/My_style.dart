0dc477f2a7b6e7347fff5b0d99796e44872a12ce
Kitthanat <kitthanat.th@gmail.com>
2020-01-19 09:47:24 +0700

    Start Git

---
+++ lib/utility/My_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  Color textcolor = Color.fromARGB(0xff, 0x00, 0x9f, 0xaf);
+  Color mainColor = Color.fromARGB(0xff, 0x88, 0xff, 0xff);
+  Color barColor = Color.fromARGB(0xff, 0x4d, 0xd0, 0xe1);
+
+  TextStyle h1Text = TextStyle(
+    fontFamily: 'Kanit',
+    color: Color.fromARGB(0xff, 0x00, 0x9f, 0xaf),
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+  );
+
+  TextStyle h2Text = TextStyle(
+    fontFamily: 'Kanit',
+    color: Color.fromARGB(0xff, 0x00, 0x9f, 0xaf),
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+  );
+
+  MyStyle();
+}
---
+++ lib/widget/authen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:joetravel/utility/My_style.dart';
+import 'package:joetravel/widget/register.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  // Field
+  // Method
+
+  Widget singInButton() {
+    return RaisedButton(
+      color: MyStyle().textcolor,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget singUpButton() {
+    return OutlineButton(
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: MyStyle().textcolor),
+      ),
+      onPressed: () {
+        print('You click Sgn Up');
+
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext buildContext) {
+          return Register();
+        });
+        Navigator.of(context).push(materialPageRoute);
+      },
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        singInButton(),
+        SizedBox(
+          width: 5.0,
+        ),
+        singUpButton(),
+      ],
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: MyStyle().textcolor)),
+            prefixIcon: Icon(
+              Icons.email,
+              color: MyStyle().textcolor,
+            ),
+            hintText: 'User : ',
+            hintStyle: TextStyle(color: MyStyle().textcolor)),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: MyStyle().textcolor)),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: MyStyle().textcolor,
+            ),
+            hintText: 'Possword : ',
+            hintStyle: TextStyle(color: MyStyle().textcolor)),
+      ),
+    );
+  }
+
+  Widget showLoGo() {
+    return Container(
+      width: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'โจ Travel',
+      style: MyStyle().h1Text,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          radius: 2.5,
+          colors: <Color>[Colors.white, MyStyle().mainColor],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            showLoGo(),
+            showAppName(),
+            userForm(),
+            passwordForm(),
+            SizedBox(
+              height: 10.0,
+            ),
+            showButton(),
+          ],
+        ),
+      ),
+    ));
+  }
+}
